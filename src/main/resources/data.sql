358c694ad7d677af7828a197a761942439066538
g00glen00b <me@g00glen00b.be>
2016-03-13 21:39:26 +0100

    Fixed database model

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO task (description, checked) VALUES
+INSERT INTO task (description, completed) VALUES
   ('Setting up our first AngularJS application', 1),
   ('Writing unit tests four our first AngularJS application', 0);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
 CREATE TABLE task (
   id          INTEGER IDENTITY PRIMARY KEY,
   description VARCHAR(64) NOT NULL,
-  checked     BIT NOT NULL);
+  completed   BIT NOT NULL);
